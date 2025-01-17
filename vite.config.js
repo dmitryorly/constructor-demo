e090fa7cee0afd3ead6b313dcdaa241359a222ea
Dmitry Orlov <dmitryorly611@gmail.com>
2025-01-17 16:12:09 +0300

    configure vite outDir

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-console.log(resolve(__dirname, 'index.html'))
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
 	base: '/constructor-demo/',
 	root: './src',
 	build: {
+		outDir: '../dist',
 		rollupOptions: {
 			input: {
 				main: './src/index.html',
